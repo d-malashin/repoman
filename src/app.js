cbc90505010c136e108cb6ffca3bc4e8dda89d33
denis-md <dmalashin@gmail.com>
2020-05-17 22:36:47 +0300

    fix for smooth scrolling

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 import "./main.scss";
 import Glide from '@glidejs/glide'
-// import portfolioSimpleLandingImage from './assets/img/thumbnails/portfolio_thumbnail_1.png'
+import smoothscroll from 'smoothscroll-polyfill';
 
 new Glide('.glide').mount()
 
-// const imagesHandler = () => {
-//   const portfolioImage1 = document.querySelector('.gallery__link--simpleLandingPage a')
-// }
+// Safari smooth scrolling
+smoothscroll.polyfill();
+
+document.querySelector('#about-link').addEventListener('click', e => {
+
+  e.preventDefault()
+  document.querySelector('#about').scrollIntoView({ 
+    behavior: 'smooth' 
+  })
+});
